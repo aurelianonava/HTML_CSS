9b15a8a18d48eb2a6573b4cad2ac233afe914ddc
Aureliano Nava <sersinfin@gmail.com>
2024-04-23 19:44:29 -0700

    Included file.js to start playing around with JavaScript.

---
+++ file.js
@@ -0,0 +1,14 @@
+//basic structure of a javascript file....
+ 
+//first Javascript program
+
+let js = "Escooby Doo";
+console.log(js);
+
+console.log("I am " + js);
+
+console.log(40 + 8 + 23 - 10);
+
+
+
+
