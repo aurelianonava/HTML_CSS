6d4c376f58c2455e48b950876a9618679cfc4b8d
Aureliano Nava <sersinfin@gmail.com>
2024-04-24 18:17:37 -0700

    Edited README.md, and file..js.

--- file.js
+++ file.js
@@ -2,12 +2,22 @@
  
 //first Javascript program
 
-let js = "Escooby Doo";
-console.log(js);
+let dog = "Escooby Doo";    //variable dog is assigned the value Escooby Doo. Quotation marks are used to denote strings.
+console.log(dog);   //prints Escooby Doo. Console.log is used to print to the console.
 
-console.log("I am " + js);
+console.log("I am " + dog); //prints I am Escooby Doo. Concatenation, the + symbol, is used to add strings together.
 
-console.log(40 + 8 + 23 - 10);
+
+
+//console.log(40 + 8 + 23 - 10);  //prints 50. Addition, the + symbol, is used to add numbers together.
+
+// let js = "amazing"; //variable
+// if (js === "amazing") {
+//     alert("js is amazing");
+// }
+
+//    alert("js is amazing");   //condition
+
 
 
 
